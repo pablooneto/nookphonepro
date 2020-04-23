fc242c63592b6044fb3e8960e88df4b53aa83537
Pablo <pablo@anoopsystems.com>
2020-04-23 12:28:03 +0200

    Updates

---
+++ precache-manifest.a9e5c5aa4b46cc5cf87ac26d283aba06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a21f3d3148cfaa1c5c3d24bf81348a62",
+    "url": "/nookphonepro/index.html"
+  },
+  {
+    "revision": "1648cf3700be1577b56d",
+    "url": "/nookphonepro/static/css/main.cf1ba6b6.chunk.css"
+  },
+  {
+    "revision": "141fd3685f324e58d277",
+    "url": "/nookphonepro/static/js/2.741c5e06.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nookphonepro/static/js/2.741c5e06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1648cf3700be1577b56d",
+    "url": "/nookphonepro/static/js/main.e316947f.chunk.js"
+  },
+  {
+    "revision": "7ed689c44263ff6acf63",
+    "url": "/nookphonepro/static/js/runtime-main.f05826ee.js"
+  }
+]);
